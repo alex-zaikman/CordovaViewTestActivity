585cd8309e7437f3e1b5d7e14bb6edb4228c89b3
alex z <t2kalex@icloud.com>
2013-09-13 13:03:36 +0300

    x

--- src/t2k/asz/controller/ClassesActivity.java
+++ src/t2k/asz/controller/ClassesActivity.java
@@ -2,9 +2,15 @@ package t2k.asz.controller;
 
 import java.util.List;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import t2k.asz.lib.model.util.CallBack;
+import t2k.asz.lib.model.util.JsonHelper;
 import t2k.asz.modle.DataModle;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -28,9 +34,9 @@ public class ClassesActivity extends Activity {
 		final GridView myGallery = (GridView)findViewById(R.id.classgallery);
 		@SuppressWarnings("unchecked")
 		final ImageAdapter adupt = new ImageAdapter(this,(List<Bitmap>) DataModle.the().rawList);
-		
+
 		myGallery.setAdapter(adupt);
-		
+
 
 	}
 
@@ -61,43 +67,63 @@ public class ClassesActivity extends Activity {
 		}
 
 		public View getView(int position, View convertView, ViewGroup parent) {
-			
+
 			ImageView view;
-			
+
 			LayoutInflater mInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-			
+
 			view = (ImageView) mInflater.inflate(R.layout.class_cell, parent, false);
-			 
+
 			view.setLayoutParams(new GridView.LayoutParams(220, 220));
 			view.setScaleType(ImageView.ScaleType.CENTER_CROP);
 			view.setPadding(8, 8, 8, 8);
 
 			view.setImageBitmap(mThumbIds.get(position));
-			
+
 			final int i = position;
-			
+
 			OnClickListener l= new OnClickListener(){
 
 				final int id = i;
-				
+
 				@Override
 				public void onClick(View v) {
-					
-					int classId = id+1;
-					
-					
-					
-					
-					
-					
-					
-					
+
+					final Integer classId = id+1;
+
+					DataModle.the().cdv.getCourse(classId+"", new CallBack(){
+						@Override
+						public void call(String msg){
+
+							try {
+								
+								JSONObject json = new JSONObject(msg);
+								DataModle.the().mdata =JsonHelper.toMap(json);  
+								Intent intent = new Intent(getApplicationContext(), ClassesActivity.class);
+								intent.putExtra("classId",classId);
+								startActivity(intent);
+
+
+							} catch (JSONException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+
+
+						}
+					}, new CallBack(){});
+
+
+
+
+
+
 				}
-				
+
 			};
 			view.setOnClickListener(l);
-			
-			
+
+
 			return view;
 		}
 
--- src/t2k/asz/controller/OverviewActivity.java
+++ src/t2k/asz/controller/OverviewActivity.java
@@ -1,22 +1,106 @@
 package t2k.asz.controller;
 
+import t2k.asz.modle.DataModle;
+import t2k.asz.modle.OverviewBean;
+import t2k.asz.modle.OverviewBean.Item;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class OverviewActivity extends Activity {
 
+	OverviewBean ob=null;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_overview);
+		
+		int classId = (Integer) getIntent().getExtras().get("classId");
+		ob = new OverviewBean(classId, DataModle.the().mdata);
+		final ListView lv = (ListView) findViewById(R.id.list);
+		
+		lv.setAdapter(new OverviewAdapter(this,R.id.txtRow ,ob));
+		
+		
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.overview, menu);
+	//	getMenuInflater().inflate(R.menu.overview, menu);
 		return true;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static class OverviewAdapter extends BaseAdapter{
+
+	    Context context; 
+		OverviewBean ob=null;
+		int layoutResourceId;
+	    
+	    public OverviewAdapter(Context context, int layoutResourceId, OverviewBean ob) {
+	    	super();
+	    	this.context=context;
+	    	this.ob=ob;
+	    	this.layoutResourceId= layoutResourceId;
+	    }
+
+		@Override
+		public int getCount() {
+			int size = this.ob.listExpanddedItemsByDFS().size();
+			return size;
+		}
+
+		@Override
+		public Object getItem(int location) {
+			return this.ob.listExpanddedItemsByDFS().get(location);
+		}
+
+		@Override
+		public long getItemId(int location) {
+			return location;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			
+			TextView row = (TextView) convertView;
+		        
+		        if(row == null)
+		        {
+		            LayoutInflater inflater = ((Activity)context).getLayoutInflater();
+		            row = (TextView) inflater.inflate(layoutResourceId, parent, false);
+		        }
+		       Item item = (Item) getItem(position);
+		        row.setText(item.getTitle());
+		        
+		        
+		        if(!item.isLeaf()){
+		        	row.setBackgroundColor(Color.BLUE);
+		        }
+			
+			
+			return row;
+		}
 	}
 
 }
