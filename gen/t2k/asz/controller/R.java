52d9900410333a16aecaedad305b5937e4945ae2
alex z <t2kalex@icloud.com>
2013-09-16 15:05:52 +0300

    v

--- gen/t2k/asz/controller/R.java
+++ gen/t2k/asz/controller/R.java
@@ -43,17 +43,19 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_classes=0x7f030000;
-        public static final int activity_lo=0x7f030001;
-        public static final int activity_main=0x7f030002;
-        public static final int activity_overview=0x7f030003;
-        public static final int class_cell=0x7f030004;
-        public static final int table_row_simple=0x7f030005;
+        public static final int activity_dl=0x7f030001;
+        public static final int activity_lo=0x7f030002;
+        public static final int activity_main=0x7f030003;
+        public static final int activity_overview=0x7f030004;
+        public static final int class_cell=0x7f030005;
+        public static final int table_row_simple=0x7f030006;
     }
     public static final class menu {
         public static final int classes=0x7f070000;
-        public static final int lo=0x7f070001;
-        public static final int main=0x7f070002;
-        public static final int overview=0x7f070003;
+        public static final int dl=0x7f070001;
+        public static final int lo=0x7f070002;
+        public static final int main=0x7f070003;
+        public static final int overview=0x7f070004;
     }
     public static final class string {
         public static final int action_settings=0x7f060002;
@@ -61,6 +63,7 @@ public final class R {
         public static final int hello_world=0x7f060003;
         public static final int loginbtn=0x7f060004;
         public static final int title_activity_classes=0x7f060005;
+        public static final int title_activity_dl=0x7f060008;
         public static final int title_activity_lo=0x7f060007;
         public static final int title_activity_main=0x7f060001;
         public static final int title_activity_overview=0x7f060006;
